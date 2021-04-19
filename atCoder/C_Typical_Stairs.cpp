3dc35f0e00f0d0fe69fb8cde66de02f78ead95b3
takeuchi-masaki <themasakitakeuchi@gmail.com>
2021-04-19 12:17:33 -0700

    stairs

--- atCoder/C_Typical_Stairs.cpp
+++ atCoder/C_Typical_Stairs.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 const int mod = 1e9+7;
 
-vector<int> st(1e5+1,-1);
+vector<long long> st(1e5+1,-1);
 int staircase(int steps){
     if(st[steps]!=-1) return st[steps];
     long long res = staircase(steps-2) + staircase(steps-1);
@@ -25,20 +25,49 @@ int main(){
     //     cout << i << " " << staircase(i) << '\n';
     // }
     long long res = 1;
-    for(int i = 1; i < n; i++){
-        int curr = 0;
-        while(i<n && !broken[i]){
-            i++;
-            curr++;
-        }
-        res*=staircase(curr);
-        res%=mod;
+    // for(int i = 1; i < n; i++){
+    //     int curr = 0;
+    //     while(i<n && !broken[i]){
+    //         i++;
+    //         curr++;
+    //     }
+    //     res*=staircase(curr);
+    //     res%=mod;
+    //     if(broken[i]){
+    //         if(i+1<=n && broken[i+1]) {
+    //             cout <<  "0\n";
+    //             return 0;
+    //         }
+    //     }
+    // }
+    int prev = 0;
+    if(broken[1]){
+        prev = 1;
+    }
+    for(int i = 2; i < n; i++){
         if(broken[i]){
-            if(i+1<=n && broken[i+1]) {
-                cout <<  "0\n";
+            if(i-prev==1){
+                cout << "0\n";
                 return 0;
             }
+            int steps = i-prev-1;
+            cout << steps << ' ' << staircase(steps) << '\n';
+            res*=staircase(steps);
+            res%=mod;
+            prev = i;
         }
+    }
+    int steps = n-prev-1;
+    cout << steps << ' ' << staircase(steps) << '\n';
+    res*=staircase(steps);
+    res%=mod;
+    cout << res << '\n';
+
+    int test[] = {21, 21, 21, 21, 11};
+    long long res2 = 1;
+    for(int i:test){
+        res2*=staircase(i);
+        res2%=mod;
     }
     cout << res << '\n';
 }
