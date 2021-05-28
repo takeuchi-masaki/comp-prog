5bc5cd554527e28ef3db0c7ad5ae23b68914fe31
takeuchi-masaki <themasakitakeuchi@gmail.com>
2021-05-28 16:28:58 -0700

    a

--- kattis/test.cpp
+++ kattis/test.cpp
@@ -1,48 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int getLongestPalindrome(string s){
-    int sz = s.size();
-    vector<int> pair_start, triplet_start; //pair triplet starting index
-    for(int i = 0; i < sz-1; i++){
-        if(s[i] == s[i+1]) {
-            pair_start.push_back(i);
-        }
+const double MAX_ERROR = 0.001;
+double findRoot(double x, int n){
+    double left = 0, right = x;
+    while(right-left > MAX_ERROR){
+            double mid = left + (right-left)/2.0;
+            double curr = 1;
+            for(int i = 0; i < n; i++){
+                curr*=mid;
+            }
+            if(curr >= x){
+                right = mid;
+            } else {
+                left = mid;
+            }
     }
-    for(int i = 0; i < sz-2; i++){
-        if(s[i] == s[i+2]) {
-            triplet_start.push_back(i);
-        }
-    }
-    int answer = 1;
-    // max number of pairs = n-1
-    for(int i = 0; i < pair_start.size(); i++){
-        int l = pair_start[i];
-        int r = l+1;
-        int currLen = 2;
-        l--, r++;
-        while(l>=0 && r<sz && s[l]==s[r]){
-            currLen+=2;
-            l--, r++;
-        }
-        answer = max(answer, currLen); 
-    // alt: if statement, store longest palindrome
-    }
-    for(int i = 0; i < triplet_start.size(); i++){
-        int l = triplet_start[i];
-        int r = l+2;
-        int currLen = 3;
-        l--, r++;
-        while(l>=0 && r<sz && s[l]==s[r]){
-            currLen+=2;
-            l--, r++;
-        }
-        answer = max(answer, currLen);
-    }
-    return answer;
+    return right;
 }
 
 int main(){
-    freopen("out.txt", "w", stdout);
-    cout << getLongestPalindrome("aaaa");
+    freopen("input.txt","r",stdin); freopen("out.txt","w",stdout);
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    cout << sqrt(.5) << endl;
+    cout << sqrt(.5) - findRoot(.5, 2) << endl;
 }
