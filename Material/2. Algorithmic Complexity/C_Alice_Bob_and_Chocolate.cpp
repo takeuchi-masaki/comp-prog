9d7de0d73d3ce23248feebafe285b6e974f9deb3
takeuchi-masaki <themasakitakeuchi@gmail.com>
2021-09-25 21:04:06 -0700

    a

---
+++ Material/2. Algorithmic Complexity/C_Alice_Bob_and_Chocolate.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // freopen("input.txt","r",stdin); freopen("out.txt","w",stdout);
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    int n; cin >> n;
+    vector<int> a(n);
+    for(int &i : a){
+        cin >> i;
+    }
+    // alice left to right, bob right to left
+    vector<int> left(n), right(n);
+    left[0] = a[0], right[0] = a[n-1];
+    for(int i = 1; i < n; i++){
+        left[i] = left[i-1] + a[i];
+        right[i] = right[i-1] + a[n-i-1];
+    }
+    // for(int i = 0; i < n; i++){
+    //     cout << left[i] << ' ';
+    // }
+    // cout << endl;
+    // for(int i = 0; i < n; i++){
+    //     cout << right[i] << ' ';
+    // }
+    // cout << endl;
+    for(int i = 0; i < n; i++){
+        
+    }
+}
