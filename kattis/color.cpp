4796e6d5e6bb60fcac65b518fb9b6e1fe64e28ab
takeuchi-masaki <themasakitakeuchi@gmail.com>
2021-11-19 22:38:32 -0800

    a

---
+++ kattis/color.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    int n, c, k; cin >> n >> c >> k;
+    vector<int> color(n);
+    for(int & i : color) cin >> i;
+    sort(color.begin(), color.end());
+    int upperLimit = color[0] + k, currCapacity = 1, ans = 1;
+    for(int i = 1; i < n; i++){
+        if(currCapacity == c || color[i] > upperLimit){
+            ans++;
+            currCapacity = 1;
+            upperLimit = color[i] + k;
+        } else {
+            currCapacity++;
+        }
+    }
+    cout << ans << '\n';
+}
