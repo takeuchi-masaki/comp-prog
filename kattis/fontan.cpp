821ab84cfa88613aa23eaa201495c7335a4ffccb
takeuchi-masaki <themasakitakeuchi@gmail.com>
2021-12-10 01:02:43 -0800

    a

--- kattis/fontan.cpp
+++ kattis/fontan.cpp
@@ -1,36 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int rMax, cMax;
-
-void dfs(vector<string>& grid, int i, int j){
-    if(i == rMax - 1 || grid[i + 1][j] == 'V') return;
-    if(grid[i + 1][j] == '.'){
-        grid[i + 1][j] = 'V';
-        dfs(grid, i + 1, j);
-    } else {
-        if(j > 0 && grid[i][j - 1] == '.'){
-            grid[i][j - 1] = 'V';
-            dfs(grid, i, j - 1);
-        }
-        if(j + 1 < cMax && grid[i][j + 1] == '.'){
-            grid[i][j + 1] = 'V';
-            dfs(grid, i, j + 1);
-        }
-    }
-}
-
 int main(){
     ios::sync_with_stdio(false); cin.tie(nullptr);
-    cin >> rMax >> cMax;
+    int rMax, cMax; cin >> rMax >> cMax;
     vector<string> grid(rMax);
     for(string& s : grid) cin >> s;
+    deque<pair<int,int>> q;
     for(int i = 0; i < rMax - 1; i++){
         for(int j = 0; j < cMax; j++){
             if(grid[i][j] != 'V') continue;
             if(grid[i + 1][j] == 'V') continue;
-            dfs(grid, i, j);
+            q.push_back({i, j});
         }
     }
+    while(!q.empty()){
+        auto p = q.front();
+        q.pop_front();
+        if(p.first == rMax - 1) continue;
+        if(grid[p.first + 1][p.second] == '.'){
+            grid[p.first + 1][p.second] = 'V';
+            q.push_back({p.first + 1, p.second});
+        } else if(grid[p.first + 1][p.second] == '#'){
+            if(p.second > 0 && grid[p.first][p.second - 1] == '.'){
+                grid[p.first][p.second - 1] = 'V';
+                q.push_back({p.first, p.second - 1});
+            }
+            if(p.second + 1 < cMax && grid[p.first][p.second + 1] == '.'){
+                grid[p.first][p.second + 1] = 'V';
+                q.push_back({p.first, p.second + 1});
+            }
+        }
+    }
+
     for(string s : grid) cout << s << '\n';
 }
--- kattis/test.cpp
+++ kattis/test.cpp
@@ -11,13 +11,17 @@ struct Node{
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(nullptr);
-    set<Node> s;
-    for(int i = 0; i < 5; i++){
-        for(int j = 0; j < 5; j++){
-            s.insert({i, j});
+    int n = 5;
+    const int INF = 1e9;
+    vector<vector<int>> dist(n, vector<int>(n, INF));
+
+    for(int k = 0; k < n; k++){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(dist[i][k] < INF && dist[k][j] < INF){
+                    dist[i][j] = min(dist[i][j], dist[i][k], dist[k][j]);
+                }
+            }
         }
-    }
-    for(Node n : s){
-        cout << n.index << ' ' << n.weight << '\n';
     }
 }
