46039238f9f29fc24ff28b53605c56aa1f620f3f
takeuchi-masaki <themasakitakeuchi@gmail.com>
2022-07-20 03:57:07 -0700

    a

--- sv/cosmos.cpp
+++ sv/cosmos.cpp
@@ -1,28 +1,29 @@
-#include <fstream>
-#include <vector>
-#include <random>
 #include <ctime>
+#include <fstream>
+#include <random>
 using namespace std;
 
-int main(){
-    srand(time(0));
-    const int T = 1e6;
-    int success = 0;
-    for(int t = 0; t < T; t++){
-        int storm = 1, ppOrDmg = 2, etc = 7;
-        for(int cnt = 0; cnt < 6; cnt++){
-            int total = storm + ppOrDmg + etc;
-            int roll = rand() % total;
-            if(roll >= storm + ppOrDmg){
-                etc--;
-            } else if(roll == 0 && storm){
-                storm--;
-            } else {
-                ppOrDmg--;
-            }
-        }
-        if(!storm && ppOrDmg <= 1) success++;
-    }
-    ofstream fout{"out.txt"};
-    fout << (long double)success / T << '\n';
+int main() {
+	srand(time(0));
+	const int T = 1e6;
+	ofstream fout { "out.txt" };
+	fout << fixed << setprecision(2);
+	for (int pp = 4; pp < 10; pp++) {
+		fout << pp << " pp:\n";
+		for (int conditions = 1; conditions <= 4; conditions++) {
+			int success = 0;
+			for (int trial = 0; trial < T; trial++) {
+				int remain = conditions;
+				int total = 10;
+				for (int i = 0; i < pp; i++) {
+					if (rand() % total < remain) {
+						remain--;
+					}
+					total--;
+				}
+				success += (remain == 0);
+			}
+			fout << conditions << " Conditions: " << (long double)success * 100 / T << "%\n";
+		}
+	}
 }
